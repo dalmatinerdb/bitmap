2312e6158352d5f9891a63b5dbf4505568478df6
Heinz N. Gies <heinz@licenser.net>
2016-12-05 13:51:01 +0100

    Some visualisation

--- src/bitmap.erl
+++ src/bitmap.erl
@@ -16,7 +16,8 @@
          unset/2,
          test/2,
          diff/2,
-         size/1
+         size/1,
+         visualize/4
         ]).
 
 -type opts() ::
@@ -97,6 +98,63 @@ diff(_BitmapA, _BitmapB) ->
 size(<<Size:64/unsigned, _/binary>>) ->
     Size.
 
+%%--------------------------------------------------------------------
+%% @doc returns the size of a bitmap.,
+%% @end
+%%--------------------------------------------------------------------
+
+
+visualize(Size, L, R, Count) ->
+    Log = trunc(math:log10(Size)) + 1,
+    Space = integer_to_list(Log),
+    S = "~" ++ Space ++ "b ",
+    %%header(Count, Space),
+    io:format(S, [0]),
+    visualize(0, Size, L, R, "~n"++S, 1, Count, Count).
+
 %%====================================================================
 %% Internal functions
 %%====================================================================
+visualize(Size, Size, _, _, _S, _N, _Line, _Count) ->
+    ok;
+visualize(P, Size, L, R, S, N, 0, Count) ->
+    io:format(S, [N * Count]),
+    visualize(P, Size, L, R, S, N + 1, Count, Count);
+visualize(P, Size, [P | L], R, S, N, Pos, Count) ->
+    cf:print("~!r<"),
+    visualize(P + 1, Size, L, R, S, N, Pos - 1, Count);
+visualize(P, Size, L, [P | R], S, N, Pos, Count) ->
+    cf:print("~!r>"),
+    visualize(P + 1, Size, L, R, S, N, Pos - 1, Count);
+visualize(P, Size, L, R, S, N, Pos, Count) ->
+    cf:print("~!g*"),
+    visualize(P + 1, Size, L, R, S, N, Pos - 1, Count).
+
+%% header(Count, Space) ->
+%%     Log = trunc(math:log10(Count))+1,
+%%     S = "~n ~" ++ Space ++ "c",
+%%     header_(S, Count, Log, 0),
+%%     io:format("~n").
+
+%% header_(S, Count, 0, _) ->
+%%     ok;
+%% header_(S, Count, Log, N)  ->
+%%     header_(S, Count div 10, Log - 1, N + 1),
+%%     io:format(S, [$\s]),
+%%     header_line(N, Count, Log).
+
+%% header_line(N, Count, 0) ->
+%%     ok;
+%% header_line(N, Count, R) ->
+%%     header_line(N, Count, R - 1),
+%%     [io:format("~" ++ integer_to_list(round(math:pow(10, N))) ++ "c", [c(N, C)])
+%%      || C <- lists:seq(0, min(Count-1, 9))].
+
+%% c(0, I) ->
+%%     [C] = integer_to_list(I),
+%%     C;
+%% c(_, 0) ->
+%%     $\s;
+%% c(_, I) ->
+%%     [C] = integer_to_list(I),
+%%     C.
